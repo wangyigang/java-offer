212f79a3be4a85b7a9a22372767887cee6e013bc
wangyigang <1010234991@qq.com>
2018-12-10 17:44:59 +0800

    上传1210部分学习数据-wangyg-b

---
+++ src/main/java/com/PartitionerComparable/ProvinceDriver.java
@@ -0,0 +1,7 @@
+package com.PartitionerComparable;
+
+public class ProvinceDriver {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/day1208/FlowMapper.java
+++ src/main/java/com/day1208/FlowMapper.java
@@ -7,18 +7,31 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlowMapper  extends Mapper<LongWritable, Text, Text, FlowBean> {
-    private Text k = new Text();
-    private FlowBean v = new FlowBean();
+    Text k = new Text();
+    FlowBean v = new FlowBean();
+
+    //重写map方法
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] strings = value.toString().split(" ");
-        
-        String phone = strings[1];
 
-        String upFlow = strings[strings.length-3];
-        String downFlow = strings[strings.length-2];
-        v.set(Long.parseLong(upFlow), Long.parseLong(downFlow));
+        /**
+         * 7 	13560436666	120.196.100.99		1116		 954			200
+         * id	手机号码		网络ip			   上行流量     下行流量     网络状态码
+         */
+        //获取一行
+        String line = value.toString();
+        //以空格切割 获取手机号 上流量 下流量
+        String[] strings = line.split("\t");
+        //获取手机号，上流量，下流量
+        String phone = strings[1];
+        String upflow = strings[strings.length-3];//上行流量
+        String downflow = strings[strings.length-2]; //下行流量
+
+        //封装对象
         k.set(phone);
-        context.write(k ,v);
+        v.set(Long.parseLong(upflow), Long.parseLong(downflow));
+
+        //写出
+        context.write(k, v);
     }
 }
